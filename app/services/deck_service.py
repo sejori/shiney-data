63a36bfa9819080e547575022e3eac7e7d33f4a0
Sebastien Ringrose <seb.ringrose@pynea.com>
2024-08-20 13:02:02 +0100

    feat: lexorank columns

--- app/services/deck_service.py
+++ app/services/deck_service.py
@@ -3,10 +3,10 @@ from dtos.deck_dtos import GetDeckWithCardsRequest
 
 def ListDeckItems(body: GetDeckWithCardsRequest):
     if body.deck_id:
-        card_results = sql(f'SELECT * FROM card WHERE deck_id = {body.deck_id}')
-        deck_results = sql(f'SELECT * FROM deck WHERE deck_id = {body.deck_id} OR parent_id = {body.deck_id}')
+        card_results = sql(f'SELECT * FROM card WHERE deck_id = {body.deck_id} ORDER BY lexorank')
+        deck_results = sql(f'SELECT * FROM deck WHERE deck_id = {body.deck_id} OR parent_id = {body.deck_id} ORDER BY lexorank')
     else:
-        card_results = sql('SELECT * FROM card')
-        deck_results = sql('SELECT * FROM deck')
+        card_results = sql('SELECT * FROM card ORDER BY lexorank')
+        deck_results = sql('SELECT * FROM deck ORDER BY lexorank')
 
     return {'deck_results': deck_results, 'card_results': card_results}
--- schema.sql
+++ schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE deck (
     deck_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     parent_id INTEGER,
+    lexorank TEXT NOT NULL,
     created_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(parent_id) references deck(deck_id)
@@ -13,6 +14,7 @@ CREATE TABLE card (
     card_id INTEGER PRIMARY KEY AUTOINCREMENT,
     content TEXT,
     deck_id integer references deck(deck_id),
+    lexorank TEXT NOT NULL,
     created_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(deck_id) references deck(deck_id)
@@ -21,13 +23,13 @@ CREATE TABLE card (
 
 
 
-INSERT INTO deck (name, parent_id) VALUES ('deck1', NULL);
-INSERT INTO deck (name, parent_id) VALUES ('deck2', NULL);
-INSERT INTO deck (name, parent_id) VALUES ('deck3', 1);
+INSERT INTO deck (name, parent_id, lexorank) VALUES ('deck1', NULL, 'a');
+INSERT INTO deck (name, parent_id, lexorank) VALUES ('deck2', NULL, 'b');
+INSERT INTO deck (name, parent_id, lexorank) VALUES ('deck3', 1, 'a');
 
-INSERT INTO card (content, deck_id) VALUES ('card 1', 1);
-INSERT INTO card (content, deck_id) VALUES ('card 2', 1);
-INSERT INTO card (content, deck_id) VALUES ('card 3', 1);
-INSERT INTO card (content, deck_id) VALUES ('card 4', 1);
+INSERT INTO card (content, deck_id, lexorank) VALUES ('card 1', 1, 'a');
+INSERT INTO card (content, deck_id, lexorank) VALUES ('card 2', 1, 'd');
+INSERT INTO card (content, deck_id, lexorank) VALUES ('card 3', 1, 'b');
+INSERT INTO card (content, deck_id, lexorank) VALUES ('card 4', 1, 'c');
 
 
